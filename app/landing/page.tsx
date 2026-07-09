ec12cd983f1c8da28d726392a6c37c15768328dd
Muhammed Haleel <muhammedhaleel21@gmail.com>
2026-07-10 00:03:48 +0530

    feat(landing): add redirect set up to signin button

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -47,7 +47,7 @@ export default function LoginPage() {
 
         <p className="mt-6 text-center text-sm text-gray-600">
           Don't have an account?{" "}
-          <a href="/register" className="text-blue-600 hover:underline">
+          <a href="/signup" className="text-blue-600 hover:underline">
             Sign Up
           </a>
         </p>
