41892e5017d731dd3a5aba7d68f11be3216da6aa
Zakin Abdul <zakinabdul.tech@gmail.com>
2026-07-10 00:00:00 +0530

    just an simple change

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function LandingPage(){
     return (
         <>
-        <Button>Landing Page</Button>
+        <Button>Landing Page1 and 2</Button>
         </>
     )
 }
